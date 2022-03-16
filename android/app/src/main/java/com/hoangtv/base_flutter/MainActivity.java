7678bf41c9b88f8dc52894633ce723782bcbab5e
Viet Hoang <hoangtv@sphinxjsc.com>
2022-03-16 10:26:07 +0700

    config flavor for Android

--- android/app/src/main/java/com/hoangtv/base_flutter/MainActivity.java
+++ android/app/src/main/java/com/hoangtv/base_flutter/MainActivity.java
@@ -1,6 +1,18 @@
 package com.hoangtv.base_flutter;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "flavor").setMethodCallHandler((call, result) -> {
+            result.success(BuildConfig.FLAVOR);
+        });
+    }
 }
